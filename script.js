49597f5ec893af6796e275d85e087097cb7dc1f0
ZayRTun <zay.t@hotmail.com>
2017-12-29 00:06:44 +0630

    audio api configured

--- script.js
+++ script.js
@@ -1,27 +1,54 @@
 let simBtn = document.getElementById('simbody');
 let huPattern = [];
 let aiPattern = [];
-const audioRed = document.getElementById('auRed');
-const audioBlue = document.getElementById('auBlue');
-const audioYellow = document.getElementById('auYellow');
-const audioGreen = document.getElementById('auGreen');
+
+const btnFreqs =[440, 600, 700, 880]
+
+var context = new AudioContext;
+var osc = context.createOscillator();
+osc.type = 'square';
+var gainNode = context.createGain();
+osc.connect(gainNode);
+gainNode.gain.value = 0.0;
+gainNode.connect(context.destination);
+osc.start(0);
+// osc.stop()
 
 
-simBtn.addEventListener('click', function(event){prsEvent(event.target);});
+console.log(context)
+
+
+simBtn.addEventListener('mousedown', function(event){
+  prsEvent(event.target);
+});
+
+simBtn.addEventListener('mouseup', function(event){
+  releaseEvent(event.target);
+});
+
+
 
 function prsEvent(element) {
   if (element.id === 'red') {
-    audioRed.play();
-    //console.log(audioRed.duration);
+    osc.frequency.setValueAtTime(btnFreqs[0], context.currentTime);
+    gainNode.gain.value = 0.1;
+    console.log('start: '+osc);
   } 
   else if (element.id === 'blue') {
-    audioBlue.play();
+    osc.frequency.setValueAtTime(btnFreqs[1], context.currentTime);
+    gainNode.gain.value = 0.1;
   }
   else if (element.id === 'yellow') {
-    audioYellow.play();
+    osc.frequency.setValueAtTime(btnFreqs[2], context.currentTime);
+    gainNode.gain.value = 0.1;
   }
   else if (element.id === 'green') {
-    audioGreen.play();
+    osc.frequency.setValueAtTime(btnFreqs[3], context.currentTime);
+    gainNode.gain.value = 0.1;
   }
-  
+}
+
+function releaseEvent() {
+  gainNode.gain.value = 0.0;
+  //console.log('stop: '+osc);
 }
