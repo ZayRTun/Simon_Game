ea43b58e1a73e0caa7dd0ecd86a0b24fe28f9f25
ZayRTun <zay.t@hotmail.com>
2018-01-11 14:57:52 +0630

    function bugs fixed

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 let simBtn = document.getElementById('simbody');
 let simCel = document.querySelectorAll('.simBtn');
 let btnTgl = document.getElementById('tglOnOf');
+let statOp = document.getElementById('statOp');
+let led = document.getElementById('led');
 let huPattern = [];
 let aiPattern = [];
 let aiPatColor = [];
 let timer1;
 let timer2;
+let timeOut;
 let onOff = false;
 let onOff1 = false;
+let onOff2 = false;
+let onOff3 = false;
 const btnFreqs =[220, 293.66, 329.63, 392.00, 110];
 
 //Web audio api
@@ -26,11 +31,12 @@ btnTgl.addEventListener('click', tglFunc);
 function tglFunc() {
   if (onOff1 === false) {
     btnTgl.style.left = '25px';
-    aiPlays();
+    statOp.innerText = '--'
     onOff1 = true;
   }
   else if(onOff1 === true) {
     btnTgl.style.left = '3px';
+    statOp.innerText = ''
     stopGame();
     onOff1 = false;
   }
@@ -42,6 +48,7 @@ simBtn.addEventListener('mousedown', function(event){
   if (onOff === false) {
     //console.log("Start the game play!");
   } else {
+    clearInterval(timeOut);
     huPattern.push(event.target.id);
     checkHuPlay(huPattern);
   }
@@ -55,6 +62,8 @@ function checkHuPlay(plays) {
     } else {
       osc.frequency.setValueAtTime(btnFreqs[4], context.currentTime);
       gainNode.gain.setValueAtTime(0.1, context.currentTime);
+
+      //insert strict mode here
       huPattern = [];
       aiPlaySteps(aiPattern);
       console.log('err1:' + aiPattern);
@@ -63,7 +72,11 @@ function checkHuPlay(plays) {
   if (huPattern.length === aiPatColor.length) {
     huPattern = [];
     onOff = false;
-    aiPlays();
+    let timer3 = setInterval(function() {
+      clearInterval(timer3);
+      aiPlays();
+    }, 500)
+    
   }
 }
 
@@ -112,7 +125,38 @@ function releaseEvent(element) {
 
 
 function startGame() {
-  aiPlays();
+  if (onOff1 === true && onOff2 === false) {
+    let i = 0;
+    let timer3 = setInterval(function() {
+      i++;
+      statOp.innerText = '';
+      let timer4 = setInterval(function() {
+        statOp.innerText = '--';
+        clearInterval(timer4);
+      }, 200)
+      if (i === 2) {
+        clearInterval(timer3);
+        aiPlays();
+      }
+    }, 500)
+    onOff2 = true;
+  }
+  else if (onOff1 === true && onOff2 === true) {
+    restartGame();
+    let i = 0;
+    let timer3 = setInterval(function() {
+      i++;
+      statOp.innerText = '';
+      let timer4 = setInterval(function() {
+        statOp.innerText = '--';
+        clearInterval(timer4);
+      }, 200)
+      if (i === 2) {
+        clearInterval(timer3);
+        //restartGame();
+      }
+    }, 500)
+  }
 }
 
 function aiPlays() {
@@ -124,9 +168,11 @@ function aiPlays() {
 }
 
 function aiPlaySteps(steps) {
-  console.log('error: '+ steps);
+  console.log(timeOut);
+  clearInterval(timeOut);
   var i = 0;
   timer1 = setInterval(topTimer, 1250);
+  
     function topTimer() {
       if (i < steps.length) {
         if (steps[i] === 0) {
@@ -158,7 +204,10 @@ function aiPlaySteps(steps) {
       else if (i === steps.length) {
         clearInterval(timer1);
         onOff = true;
+        timeOut = setInterval(timer, 5000);
       }
+      statOp.innerText = steps.length;
+      //console.log(steps.length);
     }
 }
 
@@ -211,8 +260,64 @@ function generateRandomCell() {
 
 function stopGame() {
   onOff = false;
+  onOff1 = false;
+  onOff2 = false;
+  //onOff3 = false;
   clearInterval(timer1);
+  clearInterval(timeOut);
   aiPattern = [];
   aiPatColor = [];
   huPattern = [];
+  strictMode();
+}
+
+function restartGame() {
+  clearInterval(timer1);
+  //clearInterval(timer2);
+  //clearInterval(timeOut);
+  aiPattern = [];
+  aiPatColor = [];
+  huPattern = [];
+  let timer3 = setInterval(function() {
+    aiPlays();
+    clearInterval(timer3);
+  }, 100)
+  
+}
+
+function strictMode() {
+  if (onOff1 === true) {
+    if (onOff3 === false) {
+      onOff3 = true;
+      led.style.backgroundColor = 'red';
+    }
+    else if (onOff3 === true) {
+      onOff3 = false;
+      led.style.backgroundColor = 'rgb(46, 2, 2)';
+    }
+  }
+  else if (onOff1 === false) {
+    onOff3 = false;
+    led.style.backgroundColor = 'rgb(46, 2, 2)';
+  }
+}
+
+function timer() {
+  osc.frequency.setValueAtTime(btnFreqs[4], context.currentTime);
+  gainNode.gain.setValueAtTime(0.1, context.currentTime);
+  let timer3 = setInterval(function() {
+    gainNode.gain.linearRampToValueAtTime(0.0, context.currentTime + 0.01);
+    clearInterval(timer3);
+    aiPlaySteps(aiPattern);
+  }, 1000);
+}
+
+function error() {
+  osc.frequency.setValueAtTime(btnFreqs[4], context.currentTime);
+  gainNode.gain.setValueAtTime(0.1, context.currentTime);
+
+  //insert strict mode here
+  huPattern = [];
+  aiPlaySteps(aiPattern);
+  console.log('err1:' + aiPattern); 
 }
